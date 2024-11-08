582ffcb137055d7b30d6adfdddf0a40269ffa0a2
diana12131990 <diana12131990@gmail.com>
2024-11-08 16:18:02 +0100

    Unfinish 13 and 14 and finish day 15

---
+++ 2017/Day15/day_15_1.py
@@ -0,0 +1,31 @@
+import re
+
+value_a = 679
+value_b = 771
+
+factor_a = 16807
+factor_b = 48271
+
+divisor = 2147483647
+
+def get_new_value(old_value,factor):
+    return old_value * factor % divisor
+
+def compare_two_value(a,b):
+    binary_a = bin(a)[2:].zfill(32)
+    binary_b = bin(b)[2:].zfill(32)
+    if binary_a[-16:] == binary_b[-16:]:
+        return True
+    else:
+        return False
+
+
+judge = 0
+for i in range(40000000):
+    value_a = get_new_value(value_a,factor_a)
+    value_b = get_new_value(value_b,factor_b)
+    
+    if compare_two_value(value_a,value_b):
+        judge += 1
+
+print(judge)
