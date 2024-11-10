dfabc4caf66b03eda7c2c48e383d4fb832e01294
diana12131990 <diana12131990@gmail.com>
2024-11-10 19:27:58 +0100

    Update day 16-2

--- 2017/Day16/day_16_2.py
+++ 2017/Day16/day_16_2.py
@@ -8,35 +8,34 @@ line = line.strip()
 dance_move = line.split(",")
 
 program = ''.join(chr(i) for i in range(ord('a'),ord('p')+1))
-print(program)
 
 @lru_cache(maxsize=None)
-def dancing(program):
-    for s in dance_move:
-        if s[0] == "s":         # spin
-            size = int(s[1:]) * -1
-            program = program[size:] + program[:size]
-            
-    
-        elif s[0] == "x":       # exchange
-            positions = s[1:].split("/")
-            i = int(positions[0])
-            j = int(positions[1])
+def dancing(program,s):
+
+    if s[0] == "s":         # spin
+        size = int(s[1:]) * -1
+        return program[size:] + program[:size]
         
-            lst = list(program)
-            lst[i], lst[j] = lst[j], lst[i]
-            program = ''.join(lst)
-            
-            
-        elif s[0] == "p":       # partner
-            characters = s[1:].split("/")
-            x = characters[0]
-            y = characters[1]
-            
-            program = program.replace(x,"z").replace(y,x).replace("z",y)
-            
-    return program
+    
+    elif s[0] == "x":       # exchange
+        positions = s[1:].split("/")
+        i = int(positions[0])
+        j = int(positions[1])
+        lst = list(program)
+        lst[i], lst[j] = lst[j], lst[i]
+        return ''.join(lst)
+        
+        
+    elif s[0] == "p":       # partner
+        characters = s[1:].split("/")
+        x = characters[0]
+        y = characters[1]
+        return program.replace(x,"z").replace(y,x).replace("z",y)
+    
+    
 
 for i in range(1000000000):
-    program = dancing(program)
+    for step in dance_move:
+        program = dancing(program,step)
+    
     print(i+1,":",program)
