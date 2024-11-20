cac47f700dcf985fd5456e96be140ce8fed2851e
diana12131990 <diana12131990@gmail.com>
2024-11-20 22:01:48 +0100

    Day4

---
+++ 2018/Day4/day_4_2.py
@@ -0,0 +1,54 @@
+import re
+
+f = open("day_4_input.txt","r")
+
+record_list = []
+for line in f:
+    line = line.strip()
+    record_list.append(line)
+f.close()
+
+record_list.sort()
+
+guard_record = {}
+sleep_min = -1
+wake_min = -1
+current_guard = -1
+for line in record_list:
+    time, text = line.split(']', 1)
+    time = time[1:]
+    text = text.strip()
+    
+    if "Guard" in text:
+        guard_id = re.findall(r'\d+', text)
+        current_guard = int(guard_id[0])
+        if current_guard not in guard_record:
+            guard_record[current_guard] = [0] * 60
+            
+    else:
+        time = re.findall(r'\d+', time)
+        if "falls" in text:
+            sleep_min = int(time[-1])
+            
+        elif "wakes" in text:
+            wake_min = int(time[-1])
+            if sleep_min > wake_min:
+                for i in range(sleep_min,60):
+                    guard_record[current_guard][i] += 1
+                for i in range(0,wake_min+1):
+                    guard_record[current_guard][i] += 1
+            else:
+                for i in range(sleep_min,wake_min):
+                    guard_record[current_guard][i] += 1
+                    
+most_sleep_amount = -1
+most_sleep_id = -1
+for p in guard_record:
+    if max(guard_record[p]) > most_sleep_amount:
+        most_sleep_amount = max(guard_record[p])
+        most_sleep_id = p
+        
+record = guard_record[most_sleep_id]
+max_minute = record.index(most_sleep_amount)
+
+print(most_sleep_id*max_minute)
