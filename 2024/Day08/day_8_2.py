f22080c8a1f58a57e23c927e23dbae3cbb7ea7f0
diana12131990 <diana12131990@gmail.com>
2024-12-08 20:19:06 +0100

    day 8

---
+++ 2024/Day08/day_8_2.py
@@ -0,0 +1,48 @@
+import re
+
+f = open("day_8_input.txt","r")
+
+frequency_dict = {}
+antinode_map = []
+i = 0
+for line in f:
+    line = line.strip()
+    antinode_map.append(["."]*len(line))
+    for j in range(len(line)):
+        if line[j] != ".":
+            antinode_map[i][j] = "#"
+            char = line[j]
+            if char in frequency_dict:
+                frequency_dict[char].append((i, j))
+            else:
+                frequency_dict[char] = [(i, j)]
+    i += 1
+f.close()
+
+for char in frequency_dict:
+    positions = frequency_dict[char]
+    for x in range(len(positions)-1):
+        for y in range(x+1,len(positions)):
+            d_i = positions[y][0] - positions[x][0]
+            d_j = positions[y][1] - positions[x][1]
+            
+            p1_x = positions[x][0] - d_i
+            p1_y = positions[x][1] - d_j
+            
+            while 0 <= p1_x < len(antinode_map) and 0 <= p1_y < len(antinode_map[0]):
+                antinode_map[p1_x][p1_y] = "#"
+                p1_x -= d_i
+                p1_y -= d_j                
+            
+            p2_x = positions[y][0] + d_i
+            p2_y = positions[y][1] + d_j
+            
+            while 0 <= p2_x < len(antinode_map) and 0 <= p2_y < len(antinode_map[0]):
+                antinode_map[p2_x][p2_y] = "#"
+                p2_x += d_i
+                p2_y += d_j                
+
+count = 0
+for line in antinode_map:
+    count += line.count("#")
+print(count)
